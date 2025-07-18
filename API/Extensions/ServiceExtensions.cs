ea86c2a9ec6a27447526d63cdc28cd7be3b7ef73
Vinn <vinnlauro@gmail.com>
2025-07-18 10:58:00 +0800

    create cors configuration extension

--- API/Extensions/ServiceExtensions.cs
+++ API/Extensions/ServiceExtensions.cs
@@ -2,5 +2,11 @@ namespace API.Extensions;
 
 public static class ServiceExtensions
 {
-
+    public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+            options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+        ));
 }
