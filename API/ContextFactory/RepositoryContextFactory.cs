2b09195615108baed2ce81e260515b465fa612d4
Vinn <vinnlauro@gmail.com>
2025-07-20 12:12:28 +0800

    add repository context factory

---
+++ API/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace API.ContextFactory;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    public RepositoryContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+            b => b.MigrationsAssembly("API"));
+
+        return new RepositoryContext(builder.Options);
+    }
+}
