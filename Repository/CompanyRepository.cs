d1e3727ce4a0cf8c89736ddfd0cb86454e97a42b
Vinn <vinnlauro@gmail.com>
2025-07-25 13:54:10 +0800

    add get all companies method in class

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -6,4 +6,9 @@ namespace Repository;
 public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
 {
     public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext) { }
+
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+        FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
 }
