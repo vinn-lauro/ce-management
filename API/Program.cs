e8501d6b2d13d3d1b2e307a8bfb67717e6bbd9ae
Vinn <vinnlauro@gmail.com>
2025-07-25 11:16:19 +0800

    add configuration to project

--- API/Program.cs
+++ API/Program.cs
@@ -8,6 +8,7 @@ LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentD
 
 builder.Services.ConfigureCors();
 builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
 builder.Services.AddControllers();
 
 var app = builder.Build();
