be0b43ae04a93de200561fae2aa2425eddb48c24
Vinn <vinnlauro@gmail.com>
2025-07-25 12:35:26 +0800

    add assembly reference to service controllers

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,8 @@ builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
 
 var app = builder.Build();
 
