9838651110d28c0d4d7f26beca94ac5b64671f0b
Vinn <vinnlauro@gmail.com>
2025-07-25 10:50:08 +0800

    add company repository class

---
+++ Repository/CompanyRepository.cs
@@ -0,0 +1,9 @@
+using Contracts;
+using Entities.Models;
+
+namespace Repository;
+
+public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+{
+    public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext) { }
+}
