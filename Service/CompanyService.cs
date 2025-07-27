9ee8da9406a1aed238c1e85b391c83b17e53cb97
Vinn <vinnlauro@gmail.com>
2025-07-28 07:16:44 +0800

    add null company check

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
 using Entities.Models;
 using Service.Contracts;
 using Shared.DataTransferObjects;
@@ -28,7 +29,7 @@ internal sealed class CompanyService : ICompanyService
     public CompanyDto GetCompany(Guid companyId, bool trackChanges)
     {
         var company = _repository.Company.GetCompany(companyId, trackChanges);
-
+        if (company is null) throw new CompanyNotFoundException(companyId);
         var companyDto = _mapper.Map<CompanyDto>(company);
         return companyDto;
     }
