9deccf80c418d0db42c331f76dd4434ca61515e6
Vinn <vinnlauro@gmail.com>
2025-07-27 13:53:35 +0800

    edit full address mapping

--- API/Mapping/MappingProfile.cs
+++ API/Mapping/MappingProfile.cs
@@ -9,7 +9,10 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<Company, CompanyDto>()
-        .ForMember(c => c.FullAddress,
-        opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+            .ForCtorParam("FullAddress",
+            opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+        // CreateMap<Company, CompanyDto>()
+        //     .ForMember(c => c.FullAddress,
+        //     opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
     }
 }
