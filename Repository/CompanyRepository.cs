58cb20bd8706a19ffcc051410922ee9f00c481c8
Vinn <vinnlauro@gmail.com>
2025-07-28 07:05:57 +0800

    add get company method in repository class

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -11,4 +11,7 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
         FindAll(trackChanges)
             .OrderBy(c => c.Name)
             .ToList();
+
+    public Company GetCompany(Guid companyId, bool trackChanges) =>
+        FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
 }
