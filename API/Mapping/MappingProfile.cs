6d8384a10b853261ae5bf18abff4d0d9869fb940
Vinn <vinnlauro@gmail.com>
2025-07-27 13:43:53 +0800

    add a mapping for company to company dto

---
+++ API/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using Entities.Models;
+using Shared.DataTransferObjects;
+
+namespace API.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Company, CompanyDto>()
+        .ForMember(c => c.FullAddress,
+        opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+    }
+}
