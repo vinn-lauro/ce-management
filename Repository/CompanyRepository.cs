6b87bc48afd9f910425b87db2e00c5c37251c953
Vinn <vinnlauro@gmail.com>
2025-07-29 13:06:53 +0800

    add get by ids methdod from company repository class

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -14,6 +14,9 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
             .OrderBy(c => c.Name)
             .ToList();
 
+    public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
+        FindByCondition(x => ids.Contains(x.Id), trackChanges).ToList();
+
     public Company GetCompany(Guid companyId, bool trackChanges) =>
         FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
 }
