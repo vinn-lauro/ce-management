3a042da8e6222fd14af7210ff4b51ce8c968c01b
Vinn <vinnlauro@gmail.com>
2025-07-29 12:03:22 +0800

    add create employee methdod from employee repository class

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -7,6 +7,11 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
 {
     public EmployeeRepository(RepositoryContext repositoryContext) : base(repositoryContext) { }
 
+    public void CreateEmployeeForCompany(Guid companyId, Employee employee)
+    {
+        employee.CompanyId = companyId;
+        Create(employee);
+    }
     public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges) =>
         FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges).SingleOrDefault();
 
