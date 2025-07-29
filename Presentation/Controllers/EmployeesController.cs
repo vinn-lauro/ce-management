09aba45aa4e95460d1359c213f87188aed49fc93
Vinn <vinnlauro@gmail.com>
2025-07-29 08:49:46 +0800

    add employees controller

---
+++ Presentation/Controllers/EmployeesController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[Route("api/companies/{companyId}/employees")]
+[ApiController]
+public class EmployeesController : ControllerBase
+{
+}
