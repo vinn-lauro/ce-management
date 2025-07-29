f143f04f4b7f164360c8eb37025c068943205e0d
Vinn <vinnlauro@gmail.com>
2025-07-29 13:07:25 +0800

    add get by ids methdod from company service interface

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -6,6 +6,7 @@ namespace Service.Contracts;
 public interface ICompanyService
 {
     IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
+    IEnumerable<CompanyDto> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
     CompanyDto GetCompany(Guid companyId, bool trackChanges);
     CompanyDto CreateCompany(CompanyForCreationDto company);
 }
