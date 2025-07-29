8b8f4ed23153d0128922f8f01cd9f8d907fa6ec3
Vinn <vinnlauro@gmail.com>
2025-07-29 13:10:32 +0800

    create bad request id parameter exception

---
+++ Entities/Exceptions/IdParametersBadRequestException.cs
@@ -0,0 +1,6 @@
+namespace Entities.Exceptions;
+
+public sealed class IdParametersBadRequestException : BadRequestException
+{
+    public IdParametersBadRequestException() : base("Parameter ids is null") { }
+}
