de1421e4c7730fa8a5c05187cef11bc68fd54814
Vinn <vinnlauro@gmail.com>
2025-07-29 10:02:21 +0800

    modify mapping from company to company dto for xml format

--- API/Mapping/MappingProfile.cs
+++ API/Mapping/MappingProfile.cs
@@ -9,11 +9,8 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<Company, CompanyDto>()
-            .ForCtorParam("FullAddress",
+            .ForMember(c => c.FullAddress,
             opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
-        // CreateMap<Company, CompanyDto>()
-        //     .ForMember(c => c.FullAddress,
-        //     opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
 
         CreateMap<Employee, EmployeeDto>();
     }
