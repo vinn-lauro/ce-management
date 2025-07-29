1d75d4024a8bfcac0e50281c2a7aabe374404557
Vinn <vinnlauro@gmail.com>
2025-07-29 12:32:59 +0800

    add create employee methdod from employee service class

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Contracts;
 using Entities.Exceptions;
+using Entities.Models;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 
@@ -16,6 +17,17 @@ internal sealed class EmployeeService : IEmployeeService
         _repository = repository;
         _logger = logger;
         _mapper = mapper;
+    }
+
+    public EmployeeDto CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto employeeForCreation, bool trackChanges)
+    {
+        var company = _repository.Company.GetCompany(companyId, trackChanges);
+        if (company is null) throw new CompanyNotFoundException(companyId);
+        var employeeEntity = _mapper.Map<Employee>(employeeForCreation);
+        _repository.Employee.CreateEmployeeForCompany(companyId, employeeEntity);
+        _repository.Save();
+        var employeeToReturn = _mapper.Map<EmployeeDto>(employeeEntity);
+        return employeeToReturn;
     }
 
     public EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges)
@@ -36,4 +48,6 @@ internal sealed class EmployeeService : IEmployeeService
         var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
         return employeesDto;
     }
+
+
 }
