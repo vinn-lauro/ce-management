375fec8d70b5c1f5752fcc4fd3577909aec8ef1f
Vinn <vinnlauro@gmail.com>
2025-07-29 13:15:47 +0800

    add create company collection method in company service interface

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -7,6 +7,7 @@ public interface ICompanyService
 {
     IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
     IEnumerable<CompanyDto> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
+    (IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection(IEnumerable<CompanyForCreationDto> companyCollection);
     CompanyDto GetCompany(Guid companyId, bool trackChanges);
     CompanyDto CreateCompany(CompanyForCreationDto company);
 }
