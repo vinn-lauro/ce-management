5396bc1f6290caf1fc38925a7a172b1218cebe40
Vinn <vinnlauro@gmail.com>
2025-07-29 13:04:19 +0800

    add employee for creation parameter

--- Shared/DataTransferObjects/CompanyForCreationDto.cs
+++ Shared/DataTransferObjects/CompanyForCreationDto.cs
@@ -1,3 +1,3 @@
 namespace Shared.DataTransferObjects;
 
-public record CompanyForCreationDto(string Name, string Address, string Country);
+public record CompanyForCreationDto(string Name, string Address, string Country, IEnumerable<EmployeeForCreationDto> Employees);
