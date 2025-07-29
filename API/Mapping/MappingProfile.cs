532a97e284b1ee5a850f515647125ac51346e519
Vinn <vinnlauro@gmail.com>
2025-07-29 08:56:20 +0800

    add mapping from employee to employee dto

--- API/Mapping/MappingProfile.cs
+++ API/Mapping/MappingProfile.cs
@@ -14,5 +14,7 @@ public class MappingProfile : Profile
         // CreateMap<Company, CompanyDto>()
         //     .ForMember(c => c.FullAddress,
         //     opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+
+        CreateMap<Employee, EmployeeDto>();
     }
 }
