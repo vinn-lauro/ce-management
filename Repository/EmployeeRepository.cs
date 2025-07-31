a967709495a4fba73767f01c3a4f5c19609127a7
Vinn <vinnlauro@gmail.com>
2025-07-31 08:47:10 +0800

    add delete employee method in employee repository class

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -12,6 +12,9 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
         employee.CompanyId = companyId;
         Create(employee);
     }
+
+    public void DeleteEmployee(Employee employee) => Delete(employee);
+
     public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges) =>
         FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges).SingleOrDefault();
 
