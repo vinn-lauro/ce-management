e9494fccee18ad0239bc7b4064df99c7ecdec190
Vinn <vinnlauro@gmail.com>
2025-08-01 09:40:48 +0800

    add patch update employee method in employee controller class

--- Presentation/Controllers/EmployeesController.cs
+++ Presentation/Controllers/EmployeesController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.DataTransferObjects;
@@ -51,6 +52,16 @@ public class EmployeesController : ControllerBase
     {
         if (employee is null) return BadRequest("EmployeeForUpdateDto object is null");
         _service.EmployeeService.UpdateEmployeeForCompany(companyId, id, employee, compTrackChanges: false, empTrackChanges: true);
+        return NoContent();
+    }
+
+    [HttpPatch("{id:guid}")]
+    public IActionResult PartiallyUpdateEmployeeForCompany(Guid companyId, Guid id, [FromBody] JsonPatchDocument<EmployeeForUpdateDto> patchDoc)
+    {
+        if (patchDoc is null) return BadRequest("patchDoc object sent from client is null.");
+        var result = _service.EmployeeService.GetEmployeeForPatch(companyId, id, compTrackChanges: false, empTrackChanges: true);
+        patchDoc.ApplyTo(result.employeeToPatch);
+        _service.EmployeeService.SaveChangesForPatch(result.employeeToPatch, result.employeeEntity);
         return NoContent();
     }
 }
