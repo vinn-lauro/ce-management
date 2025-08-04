8718359ab5f0971f2ced84980d4748616e14faa3
Vinn <vinnlauro@gmail.com>
2025-08-04 08:43:12 +0800

    modify employee parameters class for age restriction

--- Shared/RequestFeatures/EmployeeParameters.cs
+++ Shared/RequestFeatures/EmployeeParameters.cs
@@ -1,3 +1,8 @@
 namespace Shared.RequestFeatures;
 
-public class EmployeeParameters : RequestParameters { }
+public class EmployeeParameters : RequestParameters
+{
+    public uint MinAge { get; set; }
+    public uint MaxAge { get; set; } = int.MaxValue;
+    public bool ValidAgeRange => MaxAge > MinAge;
+}
