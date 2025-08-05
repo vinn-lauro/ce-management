3001c91b51cc3642952c6e310e6e156ef6938f81
Vinn <vinnlauro@gmail.com>
2025-08-05 09:45:17 +0800

    add order by parameter

--- Shared/RequestFeatures/RequestParameters.cs
+++ Shared/RequestFeatures/RequestParameters.cs
@@ -11,4 +11,6 @@ public abstract class RequestParameters
         get { return _pageSize; }
         set { _pageSize = (value > maxPageSize) ? maxPageSize : value; }
     }
+
+    public string? OrderBy { get; set; }
 }
