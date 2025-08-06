c160f0738a41e511babc344483c88d4f2b2f2a48
Vinn <vinnlauro@gmail.com>
2025-08-06 14:37:44 +0800

    create employee link interface

--- Contracts/IEmployeeLink.cs
+++ Contracts/IEmployeeLink.cs
@@ -1,9 +1,10 @@
 using Entities.LinkModels;
+using Microsoft.AspNetCore.Http;
 using Shared.DataTransferObjects;
 
 namespace Contracts;
 
 public interface IEmployeeLinks
 {
-    LinkResponse TryGenerateLinks(IEnumerable<EmployeeDto> employeesDto, string fields, Guid companyId, HttpContent httpContext);
+    LinkResponse TryGenerateLinks(IEnumerable<EmployeeDto> employeesDto, string fields, Guid companyId, HttpContext httpContext);
 }
