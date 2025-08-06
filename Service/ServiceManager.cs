efde105fd854ff0b722531d0299dc9df2b3e5c2c
Vinn <vinnlauro@gmail.com>
2025-08-06 09:37:07 +0800

    add datashaper dependency

--- Service/ServiceManager.cs
+++ Service/ServiceManager.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Contracts;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace Service;
 
@@ -8,10 +9,10 @@ public sealed class ServiceManager : IServiceManager
 {
     private readonly Lazy<ICompanyService> _companyService;
     private readonly Lazy<IEmployeeService> _employeeService;
-    public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager logger, IMapper mapper)
+    public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager logger, IMapper mapper, IDataShaper<EmployeeDto> dataShaper)
     {
-        _companyService = new Lazy<ICompanyService>(() => new CompanyService(repositoryManager, logger, mapper));
-        _employeeService = new Lazy<IEmployeeService>(() => new EmployeeService(repositoryManager, logger, mapper));
+        _companyService = new Lazy<ICompanyService>(() => new CompanyService(repositoryManager, logger, mapper, dataShaper));
+        _employeeService = new Lazy<IEmployeeService>(() => new EmployeeService(repositoryManager, logger, mapper, dataShaper));
     }
     public ICompanyService CompanyService => _companyService.Value;
     public IEmployeeService EmployeeService => _employeeService.Value;
