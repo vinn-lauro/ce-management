7ef3759cbd280278cbebf275a3a1667008bb4ea7
Vinn <vinnlauro@gmail.com>
2025-08-07 10:56:20 +0800

    add http options

--- Presentation/Controllers/CompaniesController.cs
+++ Presentation/Controllers/CompaniesController.cs
@@ -64,4 +64,11 @@ public class CompaniesController : ControllerBase
         await _service.CompanyService.UpdateCompanyAsync(id, company, trackChanges: true);
         return NoContent();
     }
+
+    [HttpOptions]
+    public IActionResult GetCompaniesOptions()
+    {
+        Response.Headers.Add("Allow", "GET, OPTIONS, POST, PUT, DELETE");
+        return Ok();
+    }
 }
