33960bf9d0abdec39771259f598b6fa404c08bbe
Vinn <vinnlauro@gmail.com>
2025-08-12 09:27:19 +0800

    add version configuration extension

--- API/Extensions/ServiceExtensions.cs
+++ API/Extensions/ServiceExtensions.cs
@@ -1,4 +1,5 @@
 using API.Formatter;
+using Asp.Versioning;
 using Contracts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
@@ -56,4 +57,11 @@ public static class ServiceExtensions
         });
     }
 
+    public static void ConfigureVersioning(this IServiceCollection services) =>
+        services.AddApiVersioning(options =>
+        {
+            options.ReportApiVersions = true;
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+        }).AddMvc();
 }
